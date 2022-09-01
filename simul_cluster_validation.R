65a7d020d6bd0047e8a421375832edec89e4ccf8
BadaroMath <badaromath@gmail.com>
2022-08-31 21:30:59 -0300

    Update simul_cluster_validation.R

--- simul_cluster_validation.R
+++ simul_cluster_validation.R
@@ -10,7 +10,9 @@ library("fclust")
 
 ###################### KMEANS #################################################
 ###############################################################################
-kmeans_simul <- function(A, K, iter, nstart){
+
+
+kmeans_simul <- function(A, K){
 #
 #    Arguments:
 #        A {list} -- generated cluster
@@ -19,13 +21,13 @@ kmeans_simul <- function(A, K, iter, nstart){
 #        nstart {numeric} -- start point
 #    Returns:
 #        {list} -- Rand index, correct classification proportion and information variance
-#   
-    id <- kmeans(A$X, K, iter.max = 1000, nstart = 10)$cluster
+    start_time <- Sys.time()
+    id <- kmeans(A$X, K)$cluster
     rand <- round(RandIndex(A$id, id)$AR, 3)
     prop <- round(ClassProp(A$id, id), 3)
     varinf <- round(VarInf(A$id, id), 3)
-
-    return(c(rand, prop, varinf))
+    end_time <- Sys.time()
+    return(c(rand, prop, varinf, end_time - start_time))
 }
 
 
@@ -39,7 +41,7 @@ mclust_simul <- function(A, K, model, n){
 #    Returns:
 #        {list} -- Rand index, correct classification
 #        proportion and information variance
-
+    start_time <- Sys.time()
     id <- try(Mclust(A$X, G = K, model = "VVV", verbose = FALSE)$class)
 
     if (inherits(id, what = "try-error") || (is.null(id))){
@@ -51,7 +53,8 @@ mclust_simul <- function(A, K, model, n){
     rand <- round(RandIndex(A$id, id)$AR, 3)
     prop <- round(ClassProp(A$id, id), 3)
     varinf <- round(VarInf(A$id, id), 3)
-    return(c(rand, prop, varinf))
+    end_time <- Sys.time()
+    return(c(rand, prop, varinf, end_time - start_time))
 }
 
 ###################### Espectral ##############################################
@@ -120,19 +123,19 @@ spectral_simul <- function(A, K, iter, nstart){
 #    Returns:
 #        {list} -- Rand index, correct classification 
 #        proportion and information variance
-
+    start_time <- Sys.time()
     X_spec <- spectral(A$X)
-    id <- kmeans(X_spec, K, iter.max = iter, nstart = nstart)$cluster
+    id <- kmeans(X_spec, K, iter.max = iter, nstart)$cluster
     rand <- round(RandIndex(A$id, id)$AR, 3)
     prop <- round(ClassProp(A$id, id), 3)
     varinf <- round(VarInf(A$id, id), 3)
-
-    return(c(rand, prop, varinf))
+    end_time <- Sys.time()
+    return(c(rand, prop, varinf, end_time - start_time))
 }
 
 ###################### DBSCAN #################################################
 ###############################################################################
-dbscan <- function(A, minpts, eps){
+dbscan <- function(A, minpts, eps, n){
 #    Arguments:
 #        A {list} -- generated cluster
 #        k {numeric} -- number of clusters
@@ -143,7 +146,7 @@ dbscan <- function(A, minpts, eps){
 #        proportion and information variance
     id <- dbscan::dbscan(A$X, eps, minpts)$cluster
     rand <- round(RandIndex(A$id, id)$AR, 3)
-    prop <- round(ClassProp(A$id, id), 3)
+    prop <- ifelse(round(ClassProp(A$id, id), 3) > 1, round(ClassProp(A$id, id), 3)/n, round(ClassProp(A$id, id), 3))
     varinf <- round(VarInf(A$id, id), 3)
 
     return(c(rand, prop, varinf))
@@ -155,19 +158,27 @@ dbscan_simul <- function(A, n){
 #    Returns:
 #        {list} -- Rand index, correct classification 
 #        proportion and information variance
+    start_time <- Sys.time()
     rand_dbscan <- matrix(NA, nrow = 50*length(seq(0, 2, 0.01)), ncol = 3)
     iter <-0
-    for (eps in seq(0, 2, 0.01)){
-        for (minpts in seq(1, n, 2)){
-            rand_dbscan[iter, 1] <- eps
-            rand_dbscan[iter, 2] <- minpts
-            rand_dbscan[iter, 3] <- dbscan(A,minpts, eps)[1]
-            iter <- iter + 1
-        }
+    for (minpts in seq(1, 100, 5)){
+      for (eps in seq(0, 0.5, .1)){
+        rand_dbscan[iter, 1] <- eps
+        rand_dbscan[iter, 2] <- minpts
+        rand_dbscan[iter, 3] <- dbscan(A,minpts, eps, n)[1]
+        iter <- iter + 1
+      }
     }
-    eps_opt <- rand_dbscan[which(rand_dbscan[,3] == max(na.omit(rand_dbscan[, 3])))[1], 1]
-    minpts_opt <- rand_dbscan[which(rand_dbscan[,3] == max(na.omit(rand_dbscan[, 3])))[1], 2]
-    return(dbscan(A, minpts_opt, eps_opt))
+    eps_opt <- rand_dbscan[
+        which(rand_dbscan[,3] == max(na.omit(rand_dbscan[, 3])))[1], 1
+        ]
+    minpts_opt <- rand_dbscan[
+        which(rand_dbscan[,3] == max(na.omit(rand_dbscan[, 3])))[1], 2
+        ]
+    result = dbscan(A, minpts_opt, eps_opt)
+    end_time <- Sys.time()
+    
+    return(c(result, end_time - start_time))
 }
 
 ###################### Fuzzy C-Means ##########################################
@@ -180,61 +191,53 @@ fcm_simul <- function(A, K){
 #    Returns:
 #        {numeric} -- Fuzzy Rand index, correct classification (non fuzzy labels)
 #        proportion and information variance (non fuzzy labels)
-#   
+    start_time <- Sys.time()   
     fc <- fcm(A$X, K)
     rand <- round(RI.F(A$id, fc$u, "minimum"), 3)
     prop <- round(ClassProp(A$id, fc$cluster), 3)
     varinf <- round(VarInf(A$id, fc$cluster), 3)
-
-    return(c(rand, prop, varinf))
+    end_time <- Sys.time()
+    
+    return(c(rand, prop, varinf, end_time - start_time))
 }
+
 
 ###################### Simulation ##########################################
 ###############################################################################
 
-# 1000 <= n <= 500000  -- number of points
-# 3 <= K <= 50 -- number of components
-# 1 <= p <= 10 -- number of dimensions
-# 0 <= BarOmega <= 0.8 -- value of desired average overlap.
 
-set.seed(123)
-start_time <- Sys.time()
-for (n in seq(100,500000, 100)){
-    for (K in seq(5,10000, 5)){
-        for (p in 1:10){
-            for (BarOmega in seq(0, 0.90, 0.02)){
-                tryCatch({
-                    Q <- MixSim(BarOmega = BarOmega, K = K, p = p)
-                    A <- simdataset(n = n, Pi = Q$Pi, Mu = Q$Mu, S = Q$S)
-                    cat(toString(
-                        c(n, K, p, BarOmega, kmeans_simul(A, K, 100, 10))
-                    ),
-                        file="kmeans_index.txt", append = TRUE, sep="\n")
-                    cat(toString(
-                        c(n, K, p, BarOmega, mclust_simul(A, K, "VVV", n))
-                    ),
-                        file="mclust_index.txt", append = TRUE, sep="\n")
-                    cat(toString(
-                        c(n, K, p, BarOmega, spectral_simul(A, K, 100, 10))
-                    ),
-                        file="spec_index.txt", append = TRUE, sep="\n")
-                    cat(toString(
-                        c(n, K, p, BarOmega, dbscan_simul(A, n))
-                    ),
-                        file="dbscan_index.txt", append = TRUE, sep="\n")
-                    cat(toString(
-                        c(n, K, p, BarOmega, fcm_simul(A, K))
-                    ),
-                        file="fcm_index.txt", append = TRUE, sep="\n")
-                    print(c(n, K, p, BarOmega))
-                },
-                error = function(e){
-                    str(e)
-                }
-                )
-            }
-        }
-    }
+operator <- function(BarOmega, K, p, n, file){
+  Q <- MixSim(BarOmega = BarOmega, K = K, p = p, resN = 100000)
+  A <- simdataset(n = n, Pi = Q$Pi, Mu = Q$Mu, S = Q$S)
+  print("Kmeans:")
+  cat(toString(
+    c(n, K, p, BarOmega, kmeans_simul(A, K), "K-Means")
+  ),
+  file=paste("clusters_index_", file, ".txt"), append = TRUE, sep="\n")
+  print("done:")
+  print("mclust:")
+  cat(toString(
+    c(n, K, p, BarOmega, mclust_simul(A, K, "VVV", n), "Model Based")
+  ),
+  file=paste("clusters_index_", file, ".txt"), append = TRUE, sep="\n")
+  print("done:")
+  print("spectral:")
+  cat(toString(
+    c(n, K, p, BarOmega, spectral_simul(A, K, 100, 10), "Spectral")
+  ),
+  file=paste("clusters_index_", file, ".txt"), append = TRUE, sep="\n")
+  print("done:")
+  print("dbscan:")
+  cat(toString(
+    c(n, K, p, BarOmega, dbscan_simul(A, n), "DBSCAN")
+  ),
+  file=paste("clusters_index_", file, ".txt"), append = TRUE, sep="\n")
+  print("done:")
+  print("fcm:")
+  cat(toString(
+    c(n, K, p, BarOmega, fcm_simul(A, K), "FCM")
+  ),
+  file=paste("clusters_index_", file, ".txt"), append = TRUE, sep="\n")
+  print("done:")
+  print(c(n, K, p, BarOmega))  
 }
-duration <-  start_time - Sys.time()
-print(duration)
