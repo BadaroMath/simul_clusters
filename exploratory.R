5461567de3116972d365ee716ceb5c5c4481a4e2
José Matheus Badaró <badaromath@gmail.com>
2022-09-30 16:23:33 -0300

    Add files via upload

--- exploratory.R
+++ exploratory.R
@@ -1,47 +1,146 @@
 library(ggplot2)
 library(dplyr)
 
-################ OMEGA = 0.2 ##############
+################  VARIACAO DO OMEGA ##############
 
 
-################ OMEGA = 0.5 ##############
+baromg <- read.csv("clusters_index_ BarOmega .txt", 
+                   sep= ",", 
+                   header = F)
 
 
-################ OMEGA = 0.7 ##############
-
-km <- read.csv("kmeans_index.txt", sep= ",", header = F)
-km["Metodo"] <- "K-Means" 
-mclust <- read.csv("mclust_index.txt", sep= ",", header = F)
-mclust["Metodo"] <- "Model Based"
-spec <- read.csv("spec_index.txt", sep= ",", header = F)
-spec["Metodo"] <- "Espectral"
-dbscan <- read.csv("dbscan_index.txt", sep= ",", header = F)
-dbscan["Metodo"] <- "DBSCAN"
-fcm <- read.csv("fcm_index.txt", sep= ",", header = F)
-fcm["Metodo"] <- "Fuzzy C-Means"
-
-df <- as.data.frame(rbind(km, mclust, spec, fcm))
-
-colnames(df) <- c("n", "k", "p", "Omega", "RI", "Prop", "VI", "Metodo")
+head(baromg)
+colnames(baromg) <- c("Obs", "Clusters", "Comp", 
+                      "BarOmega", "RI", "ClassProp", 
+                      "VI","TimeElapsed", "Method")
 
 
 
 
-df %>% ggplot(aes(x = Omega, y = RI, color = Metodo)) +
-  geom_line(size=1) +
-  theme_classic()
-#stat_smooth(method = "lm",
-#            formula = y ~ poly(x, 10), se = FALSE)
+baromg %>% ggplot(aes(x = BarOmega, y = RI, color = Method)) +
+  #geom_line(size=1) +
+  theme_classic()+
+  stat_smooth(method = "lm",
+              formula = y ~ poly(x, 10), se = FALSE)
 
 
-df %>% ggplot(aes(x = Omega, y = Prop, color = Metodo)) +
-  geom_line(size=1) +  theme_classic()
-#stat_smooth(method = "lm",
-#            formula = y ~ poly(x, 10), se = FALSE)
+baromg %>% ggplot(aes(x = BarOmega, y = ClassProp, color = Method)) +
+  #geom_line(size=1) +  theme_classic()+
+  stat_smooth(method = "lm",
+              formula = y ~ poly(x, 10), se = FALSE)
 
-df %>% ggplot(aes(x = Omega, y = VI, color = Metodo)) +
+baromg %>% ggplot(aes(x = BarOmega, y = VI, color = Method)) +
   #geom_line(size=1) +  theme_classic() +
   stat_smooth(method = "lm",
               formula = y ~ poly(x, 10), se = FALSE)+  
   theme_classic()
+
+
+
+################  VARIACAO DE CLUSTERS ##############
+
+
+clusters <- read.csv("clusters_index_ Clusters .txt", 
+                   sep= ",", 
+                   header = F)
+
+
+head(clusters)
+colnames(clusters) <- c("Obs", "Clusters", "Comp", 
+                      "BarOmega", "RI", "ClassProp", 
+                      "VI","TimeElapsed", "Method")
+
+
+
+
+clusters %>% ggplot(aes(x = Clusters, y = RI, color = Method)) +
+  geom_line(size=1) +
+  theme_classic()
+  #stat_smooth(method = "lm",
+  #            formula = y ~ poly(x, 8), se = FALSE)
+#
+
+clusters %>% ggplot(aes(x = Clusters, y = ClassProp, color = Method)) +
+  geom_line(size=1) +  
+  theme_classic()
+  #stat_smooth(method = "lm",
+  #            formula = y ~ poly(x, 8), se = FALSE)
+
+clusters %>% ggplot(aes(x = Clusters, y = VI, color = Method)) +
+  geom_line(size=1) +  theme_classic() +
+  #stat_smooth(method = "lm",
+  #            formula = y ~ poly(x, 10), se = FALSE)+  
+  theme_classic()
+
+
+################  VARIACAO DE CLUSTERS ##############
+
+
+comps <- read.csv("clusters_index_ Components .txt", 
+                     sep= ",", 
+                     header = F)
+
+
+head(comps)
+colnames(comps) <- c("Obs", "Clusters", "Comp", 
+                        "BarOmega", "RI", "ClassProp", 
+                        "VI","TimeElapsed", "Method")
+
+
+
+
+comps %>% ggplot(aes(x = Comp, y = RI, color = Method)) +
+  geom_line(size=1) +
+  theme_classic()
+#stat_smooth(method = "lm",
+#            formula = y ~ poly(x, 8), se = FALSE)
+#
+
+comps %>% ggplot(aes(x = Comp, y = ClassProp, color = Method)) +
+  geom_line(size=1) +  
+  theme_classic()
+#stat_smooth(method = "lm",
+#            formula = y ~ poly(x, 8), se = FALSE)
+
+comps %>% ggplot(aes(x = Comp, y = VI, color = Method)) +
+  geom_line(size=1) +  theme_classic() +
+  #stat_smooth(method = "lm",
+  #            formula = y ~ poly(x, 10), se = FALSE)+  
+  theme_classic()
+
+################  VARIACAO DE Obs ##############
+
+
+obs <- read.csv("clusters_index_ N .txt", 
+                  sep= ",", 
+                  header = F)
+
+
+head(obs)
+colnames(obs) <- c("Obs", "Clusters", "Comp", 
+                     "BarOmega", "RI", "ClassProp", 
+                     "VI","TimeElapsed", "Method")
+
+
+
+
+obs %>% ggplot(aes(x = Obs, y = RI, color = Method)) +
+  #geom_line(size=1) +
+  theme_classic()+
+  stat_smooth(method = "lm",
+              formula = y ~ poly(x, 8), se = FALSE)
+  
+
+obs %>% ggplot(aes(x = Obs, y = ClassProp, color = Method)) +
+  geom_line(size=1) +  
+  theme_classic()
+#stat_smooth(method = "lm",
+#            formula = y ~ poly(x, 8), se = FALSE)
+
+obs %>% ggplot(aes(x = Obs, y = VI, color = Method)) +
+  geom_line(size=1) +  theme_classic() +
+  #stat_smooth(method = "lm",
+  #            formula = y ~ poly(x, 10), se = FALSE)+  
+  theme_classic()
+
 
